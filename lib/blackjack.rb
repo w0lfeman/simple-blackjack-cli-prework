16b2f845393e7b72c62c5a09e99363d8db4b5e0c
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-16 23:37:53 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,7 +8,6 @@ end
 
 def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
-  card_total
 end
 
 def prompt_user
@@ -26,6 +25,7 @@ end
 def initial_round
   card_total = deal_card + deal_card
   puts display_card_total(card_total)
+  card_total
 end
 
 def hit?
