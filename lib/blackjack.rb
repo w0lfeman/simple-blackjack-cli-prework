b4d400ee8fad29ce7938b4df3cbc4d3b982d2313
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-16 23:08:53 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   1 + rand(11) 
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
