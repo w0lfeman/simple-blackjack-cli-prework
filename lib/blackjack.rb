ae8f90d7c590110f4988b73c9a982fe934d2b17a
dwolfe1987 <dwolfe1987@gmail.com>
2018-11-16 23:34:27 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,8 +23,8 @@ def end_game(card_total)
 end
 
 def initial_round
-  deal_card + deal_card
-  puts display_card_total
+  card_total = deal_card + deal_card
+  puts display_card_total(card_total)
 end
 
 def hit?
